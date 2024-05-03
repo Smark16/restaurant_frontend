f63c24e1d5f67fa8a15bfaa496d2752978d5039f
Sengendo Mark <146088416+Smark16@users.noreply.github.com>
2024-05-03 18:46:40 +0300

    Update Order.jsx

--- src/customer/Order.jsx
+++ src/customer/Order.jsx
@@ -8,7 +8,7 @@ function Order() {
 const [loading, setLoading] = useState(false)
 const [orders, setOrders] = useState([])
 const {user} = useContext(AuthContext)
-const orderPlaced = `http://127.0.0.1:8000/restaurant/userOrder/${user.user_id}`
+const orderPlaced = `https://restaurant-backend-5.onrender.com/restaurant/userOrder/${user.user_id}`
   const fetchData = async()=>{
     try{
       setLoading(true)
