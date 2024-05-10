92901184217566705b0b8306c21a05f25aeaa1c5
Sengendo Mark <146088416+Smark16@users.noreply.github.com>
2024-05-10 20:23:41 +0300

    Update customhook.jsx

--- src/customer/customhook.jsx
+++ src/customer/customhook.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../Context/AuthContext'
 function useHook(notificationOrderUrl) {
     const [orderNotify, setOrderNotify] = useState([])
     const {user} = useContext(AuthContext)
-    // const notificationOrderUrl =  `http://127.0.0.1:8000/restaurant/usermsg/${user.user_id}`
+    // const notificationOrderUrl =  `https://restaurant-backend-5.onrender.com/restaurant/usermsg/${user.user_id}`
      
  const orderMsg = async()=>{
     try{
